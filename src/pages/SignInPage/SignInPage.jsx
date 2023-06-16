879ad885f3a48da04cf304a8ae980a579f10a7ee
ElizavetaZhigunova <113633763+ElizavetaZhigunova@users.noreply.github.com>
2023-06-16 10:43:02 +0300

    16/06

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import {useForm} from 'react-hook-form'
 import { useDispatch, useSelector } from "react-redux"
 import { NavLink, useNavigate } from "react-router-dom"
 import { AuthHeader, ButtonComponent, CardComponent, InputComponent } from "../../components"
@@ -17,6 +18,17 @@ export const SignInPage = () => {
     const [password, setPassword] = useState("")
 
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        mode: "onBlur",
+        defaultValues: {
+          age: 20,
+        },
+      });
+
     useEffect(() => {
         if (isLogin) {
             navigate("/")
@@ -27,17 +39,27 @@ export const SignInPage = () => {
 
     const handlerPassword = (e) => setPassword(e.target.value)
 
-    const handlerAuth = () => {
+    const handlerAuth = (event) => {
         dispatch(signIn(email, password))
+        event.target.reset();
     }
+
 
     return (
         <div className="signInPage">
             <CardComponent>
                 <AuthHeader />
-                <InputComponent type="email" placeholder="E-mail" value={email} onChange={handlerEmail} />
+                <InputComponent type="email" placeholder="E-mail" onChange={handlerEmail} value={email} 
+                {...register("firstName", {
+                    required: "This is required",
+                    minLength: {
+                      value: 5,
+                      message: "Please enter more than 4 character",
+                    },
+                  })}/>
+                
                 <InputComponent type="password" placeholder="Пароль" value={password} onChange={handlerPassword} />
-                {user.error && <span className="error-message">Некорректно введены данные</span>}
+                {/* {user.error && <span className="error-message">Некорректно введены данные</span>} */}
                 <ButtonComponent title="Войти" onClick={handlerAuth} />
                 <div className="remember">
                     <div className="remember-me">
