863328e3d181e81b6a01c8bc58c946054ce460e5
ElizavetaZhigunova <113633763+ElizavetaZhigunova@users.noreply.github.com>
2023-06-18 14:45:37 +0300

    ыыыыыыыыыы

--- src/components/InputComponent/InputComponent.js
+++ src/components/InputComponent/InputComponent.js
@@ -6,7 +6,11 @@ export const InputComponent = (props) => {
     return (
         <div className={`inputComponent ${props.className}`}>
             <div className="inputComponentIcon">{props.icon}</div>
+<<<<<<< Updated upstream
             <input type={props.type} name={props.name} placeholder={props.placeholder} value={props.value} onChange={props.onChange} onBlur={props.onBlur} />
+=======
+            <input type={props.type} name={props.name} placeholder={props.placeholder} value={props.value} onChange={props.onChange} onBlur={props.onBlur}  />
+>>>>>>> Stashed changes
         </div>
     )
 }
